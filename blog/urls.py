fc74073c59c89d2cf724d5ac206476bbb7ad3d55
nurbek <nurbekamrullayev445@gmail.com>
2024-03-30 15:36:21 +0500

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+# blog/urls.py
+# from django.urls import path
+#
+# from .views import BlogListView, BlogDetailView
+#
+# urlpatterns = [
+#     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+#     path('', BlogListView.as_view(), name='home'),
+# ]
+
+from django.urls import path
+from .views import BlogListView, BlogDetailView  # new
+
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),  # new
+    path('', BlogListView.as_view(), name='home'),
+]
